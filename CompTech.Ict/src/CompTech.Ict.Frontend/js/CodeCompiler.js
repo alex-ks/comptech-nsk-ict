9fbeffe35e64e71c9510d34001850a827e50d416
maryartkey <maryartkey@users.noreply.github.com>
2018-02-03 21:56:43 +0700

    Add simple frontend implementation

--- CompTech.Ict/src/CompTech.Ict.Frontend/js/CodeCompiler.js
+++ CompTech.Ict/src/CompTech.Ict.Frontend/js/CodeCompiler.js
@@ -5,6 +5,94 @@ function CompileCode(code) {
     return requester.responseText;
 }
 
+function PostSession(compiledSession){
+    var requester = new XMLHttpRequest();
+    requester.open('POST', 'http://localhost:5000/api/session', false);
+    requester.setRequestHeader("Content-type", "application/json");
+    requester.send(compiledSession);
+    return requester.responseText.slice(1, -1);
+}
+
+function TmpPostSession(compiledSession){
+    var id = 1;
+    return id;
+}
+
+function GetId() {
+    var url = new URL(window.location.href);
+    return url.searchParams.get('id');
+}
+
+function isSessionFinished(session) {
+    const ops = session.operationStatus;
+    return ops.every(x => x.status === 2) || ops.some(x => x.status === 3 || x.status === 4);
+}
+
+function updateValuesTable(table) {
+    const tableNode = document.getElementById('table');
+    tableNode.innerHTML = '';
+
+    Object.keys(table).forEach(k => {
+        if (k[0] === '@') {
+            return;
+        }
+        
+        const v = table[k].value;
+
+        var iDiv = document.createElement('div');
+        iDiv.textContent = `${k} = ${v}`;
+        tableNode.appendChild(iDiv);
+    });
+}
+
+function setStatus(session) {
+    const statusNode = document.getElementById('status');
+    const ops = session.operationStatus;
+    if (ops.every(x => x.status === 2)) {
+        statusNode.textContent = 'completed';
+        return;
+    }
+
+    if (ops.some(x => x.status === 3 || x.status === 4)) {
+        statusNode.textContent = 'failed';
+        return;
+    }
+
+    statusNode.textContent = 'in progress';
+}
+
+function fetchSessionData() {
+    var xhr = new XMLHttpRequest();
+  
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== XMLHttpRequest.DONE) {
+          return;
+      }
+
+      var sessionStatus = null;
+      if (this.status == 200 && this.responseText) {
+        sessionStatus = JSON.parse(this.responseText); 
+        updateValuesTable(sessionStatus.mnemonicsTable);
+        setStatus(sessionStatus);
+      }
+      
+      if (!sessionStatus || !isSessionFinished(sessionStatus)) {
+        setTimeout(fetchSessionData, 1000);
+      }
+    }
+
+    xhr.open('GET', 'http://localhost:5000/api/session/' + GetId(), true);
+    xhr.send();
+  }
+
+function initSessionPage(SessionId){
+    var id = GetId();
+    var idElement = document.getElementById('sessionId');
+    idElement.textContent = id;
+
+    fetchSessionData(id);
+}
+
 function GetTextareaValue(id) {
     return document.getElementById(id).value;
 }
@@ -20,5 +108,8 @@ function GetInputValue(id) {
 function HandleCompile() {
     var text = GetTextareaValue("source");
     var compiled = CompileCode(text);
-    SetTextareaValue("compiled", compiled);
+    var id = PostSession(compiled);
+
+    console.log("Compiled session: ", compiled);
+    window.location.href = 'session.html?id=' + id;
 }
